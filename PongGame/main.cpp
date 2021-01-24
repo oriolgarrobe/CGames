11081f9fab4858e09443dc7dc68fcaca725cd5fa
Oriol Garrobé <oriol.garrobe@gmail.com>
2021-01-24 19:45:38 +0100

    "Added cGameManager Class"

--- PongGame/main.cpp
+++ PongGame/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -101,9 +103,46 @@ public:
 		o << "Paddle [" << c.x << "," << c.y << "]";
 		return o;
 	}
-
 };
 
+class cGameManager
+{
+private:
+	int width, height;
+	int score1, score2;
+	char up1, down1, up2, down2;
+	bool quit;
+	cBall * ball;
+	cPaddle *player1;
+	cPaddle *player2;
+public:
+	cGameManager{ int w, int h }
+	{
+		srand(time(NULL));
+		quit = false;
+		up1 = "w"; up2 = "i";
+		down1 = "s"; down2 = "k";
+		score1 = score2 = 0;
+		width = w; height = h;
+		ball = new cBall(w / 2, h / 2);
+		player1 = new cPaddle(1, h / 2 - 3);
+		player2 = new cPaddle(w - 2, h / 2 - 3);
+	}
+	~cGameManager()
+	{
+		delete ball, player1, player2;
+	}
+	void ScoreUp(cPaddle * player)
+	{
+		if (player == player1)
+			score1++;
+		else if (player == player2)
+			score2++;
+		ball->Reset();
+		player1->Reset();
+		player2->Reset();
+	}
+};
 int main()
 {
 	cPaddle p1(0, 0);
